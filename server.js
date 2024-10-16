e4013f8ac0d7e6656565d193c4830e51dda30ceb
Anurag <anuragsonkar053@gmail.com>
2024-10-16 13:07:05 +0530

    initial-setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
+const PORT = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+
+// routes
+app.get('/' , async(req,res)=>{
+    res.send("H550")
+})
+
+// listening PORT
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
