61c0cc34140ce41669681ad2d9862e841ab09ee5
Anurag <anuragsonkar053@gmail.com>
2024-10-16 13:27:06 +0530

    mongodb-setup

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const URL = process.env.MONGODB_LOCAL_URL
+// connection
+mongoose.connect(URL);
+
+// setup listners
+const db = mongoose.connection;
+
+db.on("connected", () => console.log(`Connected To MongoDb Server`));
+db.on("error", (error) => console.log(error.message));
+db.on("disconnected", () => console.log("MongoDb disconnected"));
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const db = require("./config/db");
+const dotenv = require("dotenv");
+dotenv.config();
 const cors = require('cors')
 const PORT = process.env.PORT || 5000;
 
